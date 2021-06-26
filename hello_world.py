10dbd375fddc79642e5fa3595442e90a4c5be863
Harutee7 <86536337+Harutee7@users.noreply.github.com>
2021-06-26 22:37:13 +0100

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,30 @@
+
+print ("Hello Python World!")
+print ("Hello Python Crash Course world!")
+print ("Hello There!")
+print ("greeting_message!")
+print ("s_n!")
+print ("1 to 100!")
+print ("hello world")
+print ("hello python crash course reader")
+print ("hello world.py")
+messsage="hello python crash course reader"
+print (messsage)
+first_name="ada"
+last_name="lovelace"
+full_name="f{first_name}"
+print(full_name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
